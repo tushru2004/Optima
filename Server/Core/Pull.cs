210ce5fb67d4d78ce8552dd747d8e21e9bed60fb
Tushar Timande <tushru2004@gmail.com>
2025-05-11 15:44:39 +0200

    Adding Server push code

---
+++ Server/Core/Pull.cs
@@ -0,0 +1,6 @@
+namespace Server;
+
+public class Pull
+{
+    
+}
---
+++ Server/Core/UpdateManager.cs
@@ -0,0 +1,57 @@
+using Server.Nats;
+
+namespace Server.Core;
+
+using System.IO;
+
+
+public class UpdateManager
+{       
+    private static DateTime _lastRead = DateTime.MinValue;
+
+    public void WatchGatewayConfigs()
+    {
+        var filePath = "Util/ALLGatewayConfigs.json";
+        var directoryPath = Path.GetDirectoryName(filePath);
+
+        if (directoryPath == null || !Directory.Exists(directoryPath))
+        {
+            Console.WriteLine($"Directory '{directoryPath}' does not exist.");
+            return;
+        }
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File '{filePath}' does not exist. Watching for changes when it is created...");
+        }
+
+        var watcher = new FileSystemWatcher
+        {
+            Path = directoryPath,
+            Filter = Path.GetFileName(filePath),
+            NotifyFilter = NotifyFilters.LastWrite
+        };
+
+        watcher.Changed += (sender, e) =>
+        {
+            var currentChange = File.GetLastWriteTime(e.FullPath);
+
+            if (Math.Abs((currentChange - _lastRead).TotalMilliseconds) > 1000)
+            {
+                _lastRead = currentChange;
+                Console.WriteLine($"File '{e.FullPath}' updated. Last write time: {currentChange}");
+                Console.WriteLine($"Time to push updates to ALL gateways");
+                new NatsHelper().PushConfigs();
+            }
+
+        };
+
+        watcher.EnableRaisingEvents = true;
+        Console.WriteLine($"Watching for changes in file: {filePath}");
+    }
+}
+
+
+
+
+
