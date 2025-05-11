eb7d26264cfa9aca178371e73a77618f6a1865f2
Tushar Timande <tushru2004@gmail.com>
2025-05-11 17:11:08 +0200

    Adding necessary files to git

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,8 +1,31 @@
-﻿namespace MyProject;
-class Program
+﻿using NATS.Client;
+using System.Text;
+using Utf8Json;
+
+namespace Gateway;
+
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var gatewayId = Environment.GetEnvironmentVariable("GATEWAY_ID")
+                        ?? throw new InvalidOperationException("Environment variable 'GATEWAY_ID' is not set.");
+        Console.WriteLine("Gateway is subscribing to messages...");
+
+        var options = ConnectionFactory.GetDefaultOptions();
+        options.Url = "nats://nats:4222"; // NATS server URL
+
+        using (var connection = new ConnectionFactory().CreateConnection(options))
+        {
+            var subject = $"gateway.{gatewayId}.messages";
+            connection.SubscribeAsync(subject, (sender, msgArgs) =>
+            {
+                string prettyJson = JsonSerializer.PrettyPrint(msgArgs.Message.Data);
+                var receivedMessage = Encoding.UTF8.GetString(msgArgs.Message.Data);
+                Console.WriteLine($"Message received on subject '{subject}': {prettyJson}");
+            });
+
+            while (true) Thread.Sleep(5000); // Keep the application running
+        }
     }
 }
---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using NATS.Client;
+using Server.Core;
+using Server.Util;
+using Server.Util.Models;
+
+namespace Server;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Server is publishing a message...");
+        
+        new UpdateManager().WatchGatewayConfigs();
+        // Keep the application alive
+        Console.WriteLine("Server is running. Press Ctrl+C to exit...");
+        while (true)
+        {
+            Thread.Sleep(1000); // Keeps the app alive
+        }
+    }
+}
